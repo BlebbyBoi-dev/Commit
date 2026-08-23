837a8da03e106e2e569a038e673cbedfe9d01190
BlebbyBoi-dev <blebbyboi235@gmail.com>
2026-08-23 20:05:22 +0800

    Add dash and facing-based movement
    The player movement script now tracks the facing direction and applies a short backward dash when Dash is pressed, while updating dashing state during horizontal movement. This introduces a more responsive movement feel and keeps the character momentum consistent on both floor and air states. The project time tracker was also updated to reflect the recent work.

--- player_movement.gd
+++ player_movement.gd
@@ -4,7 +4,9 @@ var gravity = 400
 var accel = 500
 var jump_power = -20000
 var max_speed = 1000
-var dir = 0
+var dashpower = 1200
+var PlayerFacing = "right"
+var dashing = false
 
 func _physics_process(delta: float) -> void:
 	# GRAVITY
@@ -13,11 +15,17 @@ func _physics_process(delta: float) -> void:
 	# MOVEMENT
 	if Input.is_action_pressed("Left") and is_on_floor():
 		velocity.x += accel * delta * -1
+		dashing = false
+	elif Input.is_action_pressed("Right") and !is_on_floor():
+		dashing = true
 	if Input.is_action_just_released("Left"):
 		velocity.x = 0
 	
 	if Input.is_action_pressed("Right") and is_on_floor():
 		velocity.x += accel * delta
+		dashing = false
+	elif Input.is_action_pressed("Right") and !is_on_floor():
+		dashing = true
 	if Input.is_action_just_released("Right"):
 		velocity.x = 0
 
@@ -30,9 +38,28 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y += jump_power * delta
 	
-
+	# BACKSTEP
+	# Get the direction that the player is facing
+	if Input.is_action_pressed("Right"):
+		PlayerFacing = "right"
+	elif Input.is_action_pressed("Left"):
+		PlayerFacing = "left"
 	
+	# Launch the player backward
+	if Input.is_action_just_pressed("Dash") and PlayerFacing == "right":
+		velocity.x = dashpower * -1
+		dashing = true
 		
+	if velocity.x < 0 and dashing == true:
+		velocity.x += 50
+	
+	if Input.is_action_just_pressed("Dash") and PlayerFacing == "left":
+		velocity.x = dashpower
+		dashing = true
+		
+	if velocity.x > 0 and dashing == true:
+		velocity.x -= 50
+	
 	
 	move_and_slide()
-	print (velocity, dir)
+	print (velocity, PlayerFacing)
