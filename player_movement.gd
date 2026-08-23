d71c4e6ba43b949ed231067a370afcb4e7f31a52
BlebbyBoi-dev <blebbyboi235@gmail.com>
2026-08-23 21:12:23 +0800

    Revamped backstep move
    The project now includes a Windows desktop export preset, and the level platforms use the icon texture for visual consistency. Player movement was revised to use a directional facing value and a dedicated backstep/dash helper, with adjusted dash strength and decay; project time tracking was also updated.

--- player_movement.gd
+++ player_movement.gd
@@ -4,29 +4,31 @@ var gravity = 400
 var accel = 500
 var jump_power = -20000
 var max_speed = 1000
-var dashpower = 1200
-var PlayerFacing = "right"
+var dashpower = 700
+var PlayerFacing = 0
 var dashing = false
+var codeworking = false
 
 func _physics_process(delta: float) -> void:
+	
+	# very important code
+	if 1+1 == 2:
+		codeworking = true
+	
 	# GRAVITY
 	velocity.y += gravity * delta
 
 	# MOVEMENT
-	if Input.is_action_pressed("Left") and is_on_floor():
+	if Input.is_action_pressed("Left") and is_on_floor() and !dashing:
+		PlayerFacing = 1
 		velocity.x += accel * delta * -1
-		dashing = false
-	elif Input.is_action_pressed("Right") and !is_on_floor():
-		dashing = true
-	if Input.is_action_just_released("Left"):
+	if Input.is_action_just_released("Left") and !dashing:
 		velocity.x = 0
 	
-	if Input.is_action_pressed("Right") and is_on_floor():
+	if Input.is_action_pressed("Right") and is_on_floor() and !dashing:
+		PlayerFacing = -1
 		velocity.x += accel * delta
-		dashing = false
-	elif Input.is_action_pressed("Right") and !is_on_floor():
-		dashing = true
-	if Input.is_action_just_released("Right"):
+	if Input.is_action_just_released("Right") and !dashing:
 		velocity.x = 0
 
 	if velocity.x <= -1 * max_speed:
@@ -38,28 +40,21 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y += jump_power * delta
 	
-	# BACKSTEP
-	# Get the direction that the player is facing
-	if Input.is_action_pressed("Right"):
-		PlayerFacing = "right"
-	elif Input.is_action_pressed("Left"):
-		PlayerFacing = "left"
-	
-	# Launch the player backward
-	if Input.is_action_just_pressed("Dash") and PlayerFacing == "right":
-		velocity.x = dashpower * -1
-		dashing = true
-		
-	if velocity.x < 0 and dashing == true:
-		velocity.x += 50
-	
-	if Input.is_action_just_pressed("Dash") and PlayerFacing == "left":
-		velocity.x = dashpower
-		dashing = true
-		
-	if velocity.x > 0 and dashing == true:
-		velocity.x -= 50
-	
-	
+	# CALL BACKSTEP
+	if Input.is_action_just_pressed("Dash") and !dashing:
+		backstep()
+	# Decay backstep
+	if velocity.x < 0 and dashing: 
+		velocity.x += 20
+	if velocity.x > 0 and dashing: 
+		velocity.x -= 20
+	if velocity.x == 0:
+		dashing = false
+
 	move_and_slide()
 	print (velocity, PlayerFacing)
+	
+func backstep(): #backstep yippee
+	dashing = true
+	velocity.x = dashpower * PlayerFacing
+	move_and_slide()
